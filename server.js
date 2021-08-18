e4027b2e6e2cdbb0ff57c7439b24513998ede680
dave <dsersch640@gmail.com>
2021-08-18 10:42:36 -0400

    database test

--- server.js
+++ server.js
@@ -2,8 +2,41 @@ const dotenv = require('dotenv');
 dotenv.config({path: './config.env'})
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose')
 const PORT = process.env.PORT;
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+)
 
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Database connection successful...")
+})
+
+const shotSchema = new mongoose.Schema({
+    shotType: String,
+    club: String,
+    targetHit: Boolean,
+})
+
+const Shot = mongoose.model('Shot', shotSchema);
+
+const testShot = new Shot({
+    shotType: 'chunked',
+    club: '8 iron',
+    targetHit: false,
+})
+
+testShot.save().then(doc => {
+    console.log(doc)
+}).catch(err => {
+    console.log(err)
+})
 
 const app = express();
 
@@ -17,6 +50,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, (err) => {
-    err ? console.log(err) :
-console.log(`Server running on ${PORT}...`)
+    err || console.log(`Server running on ${PORT}...`)
 })
